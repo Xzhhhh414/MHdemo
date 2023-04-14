6c51914e1e158e9346bc1b6aba34ac213aa4225d
Xzhhhh <john.zh@outlook.com>
2023-04-15 02:03:15 +0800

    example

---
+++ mhdemo/Assets/ParadoxNotion/_Resources/FlowCanvas Resources/Example Scenes/SpaceInvaders by 'AndyGFX'/Scripts/ClonePrefab.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ClonePrefab : MonoBehaviour
+{
+    public void Clone(GameObject prefab, Vector3 position) {
+        Instantiate(prefab, position, Quaternion.identity);
+    }
+}
---
+++ mhdemo/Assets/ParadoxNotion/_Resources/FlowCanvas Resources/Example Scenes/SpaceInvaders by 'AndyGFX'/Scripts/ParticleSystemAutoDestroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ParticleSystemAutoDestroy : MonoBehaviour
+{
+    private ParticleSystem ps;
+
+    public void Start() {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    public void Update() {
+        if ( ps ) {
+            if ( !ps.IsAlive() ) {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
